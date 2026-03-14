b1e68ca09b888cae711df2fd99d48b8ed807edb3
Kunal074 <kunalsahu232777@gmail.com>
2026-03-14 20:04:00 +0530

    Auto migrate on startup

--- server.js
+++ server.js
@@ -2,13 +2,19 @@
 require("dotenv").config();
 require("express-async-errors");
 
-const app = require("./src/app");
+const { execSync } = require("child_process");
+const app          = require("./src/app");
 const { connectDB } = require("./src/config/db");
 
 const PORT = process.env.PORT || 5000;
 
 const start = async () => {
   try {
+    // Auto migrate on startup
+    console.log("🔄 Running migrations...");
+    execSync("npx prisma migrate deploy", { stdio: "inherit" });
+    console.log("✅ Migrations done!");
+
     await connectDB();
     app.listen(PORT, () => {
       console.log(`
