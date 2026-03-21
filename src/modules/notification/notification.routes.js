97ac0e956475d7d62e933556fda92245e7c8cd70
Kunal074 <kunalsahu232777@gmail.com>
2026-03-21 12:37:59 +0530

    Fix - remove auth from register token

--- src/modules/notification/notification.routes.js
+++ src/modules/notification/notification.routes.js
@@ -5,7 +5,7 @@ const { requireStoreOwner, requireRole } = require("../../middleware/roleCheck")
 const ctrl             = require("./notification.controller");
 
 // App se token register karo
-router.post("/register-token",  authenticate,    ctrl.registerToken);
+router.post("/register-token",   ctrl.registerToken);
 
 // Store Owner — notification bhejo
 router.post("/send",            authenticate, requireStoreOwner, ctrl.sendNotification);
